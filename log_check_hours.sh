c265e032efa868e4085edae23bb2f64fee6e6dd5
Paul Stivers <paul.stivers@fidelitylife.com>
2019-12-10 00:33:52 -0800

    improve var names

--- log_check_hours.sh
+++ log_check_hours.sh
@@ -7,14 +7,14 @@ echo; echo "filename and hours for which there are no log entries"; echo
 echo "   filename      UTC  PST"
 sleep 1
 for file in $(ls -1 | grep 'gz'); do
-  for hour in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do 
-    a=$(zcat $file | grep "T${hour}:")
+  for utc in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do 
+    a=$(zcat $file | grep "T${utc}:")
     if [ -z "$a" ]; then
-      utc=$(( 10#$hour - 8 ))
-      if (( $utc < 0 )); then
-        utc=$(( 24 + utc ))
+      pst=$(( 10#$utc - 8 ))
+      if (( $pst < 0 )); then
+        pst=$(( 24 + pst ))
       fi
-    echo "$file   $hour   $utc"
+    echo "$file   $utc   $pst"
     fi 
   done
   echo
