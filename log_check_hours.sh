fd90d387bb1d435a8d8456008fedc28848c2afc3
Paul Stivers <paul.stivers@fidelitylife.com>
2020-04-13 16:03:36 -0700

    white space change?

--- log_check_hours.sh
+++ log_check_hours.sh
@@ -7,7 +7,7 @@ echo; echo "filename and hours for which there are no log entries"; echo
 echo "   filename      UTC  PST"
 sleep 1
 for file in $(ls -1 | grep 'gz'); do
-  for utc in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do 
+  for utc in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24; do
     a=$(zcat $file | grep "T${utc}:")
     if [ -z "$a" ]; then
       pst=$(( 10#$utc - 8 ))
